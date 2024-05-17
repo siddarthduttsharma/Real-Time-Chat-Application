8761625d1395191a9e7cf1916f68cf034f4eb646
Siddarth Dutt <siddarthduttsharma@gmail.com>
2024-05-17 18:45:22 +0530

    Add project files.

---
+++ API/Controllers/ChatController.cs
@@ -0,0 +1,31 @@
+﻿using API.Dtos;
+using API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly ChatService _chatService;
+        public ChatController(ChatService chatService)
+        {
+            _chatService = chatService;
+        }
+
+        [HttpPost("register-user")]
+        public IActionResult RegisterUser(UserDto model)
+        {
+            if (_chatService.AddUserToList(model.Name))
+            {
+
+                // 204 status code
+                return NoContent();
+            }
+
+            return BadRequest("This name is taken please choose another name");
+        }
+    }
+}
---
+++ API/Hubs/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using API.Services;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace API.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly ChatService _chatService;
+        public ChatHub(ChatService chatService)
+        {
+            _chatService = chatService;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "ComeChat");
+            await Clients.Caller.SendAsync("UserConnected");
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "ComeChat");
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
